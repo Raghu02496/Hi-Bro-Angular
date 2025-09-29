38d1fa3b94609c29628e3e1893026e4c59457474
Raghu02496 <lawlieta023@gmail.com>
2025-09-29 18:46:35 +0530

    api service with endpoints added

---
+++ src/app/api-service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  serverAddress  = 'http://localhost:3000'
+  constructor(private httpClient : HttpClient){}
+
+  getTodo(request: any){
+    return this.httpClient.post(this.serverAddress+'/todo/getTodo',request)
+  }
+
+  addTodo(request: any){
+    return this.httpClient.post(this.serverAddress+'/todo/addTodo',request)
+  }
+}

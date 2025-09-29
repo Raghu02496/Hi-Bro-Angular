aa4fd0b3b12c93b693bffa4369e2011703500498
Raghu <raghuramap41@gmail.com>
2025-09-29 18:47:41 +0530

    Todo add and fetch added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,60 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api-service';
+import { share, shareReplay } from 'rxjs';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Hi-Bro-Angular');
+  todoFormGrp !: FormGroup
+  constructor(
+    private apiService:ApiService,
+    private fb : FormBuilder
+  ){
+    this.todoFormGrp = fb.group({
+      string : ['',Validators.required],
+      done : [false]
+    })
+  }
+
+  ngOnInit(){
+    this.fetchTodo()
+  }
+
+  fetchTodo(){
+    let request = {
+      limit : 10,
+      page : 0
+    }
+    
+    this.apiService.getTodo(request).pipe(shareReplay(1)).subscribe({
+      next : (res)=>{
+        console.log(res,'api status')
+      }
+    })
+  }
+
+  addTodo(){
+    if(this.todoFormGrp.valid){
+      let request = {
+        string :this.todoFormGrp.get('string')?.value,
+        done : this.todoFormGrp.get('done')?.value
+      }
+
+      this.apiService.addTodo(request).subscribe({
+        next : (res:any)=>{
+          if(res.ok){
+            this.todoFormGrp.reset()
+          }
+        }
+      })
+    }
+  }
+
 }

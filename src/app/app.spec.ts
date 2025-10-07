77ecc7def858e003143236b29485b6083463a8d3
Raghu <raghuramap41@gmail.com>
2025-10-07 11:46:48 +0530

    unit tests updated

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App,HttpClientModule],
     }).compileComponents();
   });
 
@@ -18,6 +19,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hi-Bro-Angular');
+    expect(compiled.querySelector('.bro-container')?.textContent).toContain('Hi bro');
   });
 });

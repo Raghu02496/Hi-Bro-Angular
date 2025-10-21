69f4a100b471ed3154c5d94cc20b6f66a4e9073b
Raghu02496 <lawlieta023@gmail.com>
2025-10-21 16:37:00 +0530

    case detail interface added

--- src/app/detective.interface.ts
+++ src/app/detective.interface.ts
@@ -2,3 +2,18 @@ export interface conversation{
     role : 'user' | 'assistant'
     content : string
 }
+
+export interface caseDetails{
+    title : string,
+    description : string,
+    suspects : Array<suspect>,
+    cluePool : Array<string>,
+    lastSummaryCount : Number,
+    summary : string
+}
+
+export interface suspect{
+    _id : string,
+    name : string,
+    role : string
+}

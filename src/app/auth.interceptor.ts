e765bba70458f0c1d27607a4e4c1d41b3b108205
Raghu02496 <lawlieta023@gmail.com>
2025-10-23 22:18:57 +0530

    interceptor added to send jwt tokens

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandlerFn, HttpRequest, HttpResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { ApiService } from './api-service';
+
+export function authInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
+  
+    const apiService = inject(ApiService)
+    let cloned = req
+    
+    const isProtectedRoute = req.url.split('/')[3] === 'protected'
+    
+    if(isProtectedRoute){
+      cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${apiService.key}`
+        }
+      });
+    }
+  
+    return next(cloned)
+  }

095ea86caa7704f9d0a1ceccde8015f06f751133
Raghu02496 <lawlieta023@gmail.com>
2025-10-23 22:20:07 +0530

    login added

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,19 +1,47 @@
-import { Component, EmbeddedViewRef, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, EmbeddedViewRef } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { ApiService } from '../api-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterLink],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './auth.html',
   styleUrl: './auth.scss'
 })
 export class Auth {
-  @ViewChild('login',{read : TemplateRef}) loginTemplate !: TemplateRef<any>
-  @ViewChild('templateContainer',{read : ViewContainerRef}) templateContainer !: TemplateRef<any>
-  loginEmbeddedRef !: EmbeddedViewRef<any>
 
-  ngAfterViewInit(){
-    this.templateContainer.createEmbeddedView(this.loginTemplate);
+  loginFrmGrp !: FormGroup
+  constructor(
+    private fb : FormBuilder,
+    private apiService : ApiService,
+    private router : Router
+  ){
+    this.loginFrmGrp = fb.group({
+      userName : ['',Validators.required],
+      password : ['',Validators.required]
+    })
+  }
+
+  login(){
+    if(this.loginFrmGrp.valid){
+      let request = {
+        userName : this.loginFrmGrp.get('userName')?.value.trim(),
+        password : this.loginFrmGrp.get('password')?.value.trim()
+      }
+      this.apiService.login(request).subscribe({
+        next : (res:any)=>{
+          if(res.ok){
+            this.apiService.key = res.data
+            this.router.navigate(['/detective'])
+          }
+        }
+      })
+    }
   }
   
 }

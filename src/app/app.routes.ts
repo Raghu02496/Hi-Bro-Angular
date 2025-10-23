7696ad66fb9765dc9876d3a6545e172cd2c45326
Raghu02496 <lawlieta023@gmail.com>
2025-10-23 18:58:10 +0530

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Game } from './game/game';
+import { Auth } from './auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '', redirectTo : 'login', pathMatch: 'full'
+    },
+    {
+        path : 'detective', component : Game
+    },
+    {
+        path : 'login', component : Auth
+    }
+];

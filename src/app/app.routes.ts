bedfd4991231cbaaf7a816cf316ccfb5265647c1
Raghu02496 <lawlieta023@gmail.com>
2025-10-23 22:55:05 +0530

    route guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { Game } from './game/game';
 import { Auth } from './auth/auth';
+import { inject } from '@angular/core';
+import { ApiService } from './api-service';
 
 export const routes: Routes = [
     {
         path : '', redirectTo : 'login', pathMatch: 'full'
     },
     {
-        path : 'detective', component : Game
+        path : 'detective', component : Game, canMatch : [
+            ()=>{
+                const apiService = inject(ApiService);
+                const router = inject(Router);
+                router.navigate(['/login']);
+                return apiService.key ? true : false
+            }
+        ]
     },
     {
         path : 'login', component : Auth

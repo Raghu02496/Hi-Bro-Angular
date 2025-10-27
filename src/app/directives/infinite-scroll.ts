20b1324ff56385ae52dbe30a6b42868c0dccfa2f
Raghu02496 <lawlieta023@gmail.com>
2025-10-27 13:19:24 +0530

    directive added for infinite scrolling

---
+++ src/app/directives/infinite-scroll.ts
@@ -0,0 +1,39 @@
+import { Directive, ElementRef, Input, output } from '@angular/core';
+
+@Directive({
+  selector: '[InfiniteScroll]'
+})
+export class InfiniteScroll {
+  intersected = output<void>();
+  observer !: IntersectionObserver;
+  target !: ElementRef<HTMLElement>
+  constructor(
+    eleRef : ElementRef<HTMLElement>
+  ) 
+  {
+    this.target = eleRef
+  }
+
+  ngAfterViewInit(){
+    this.observer = new IntersectionObserver((entries)=>{
+      for(let entry of entries){
+        if(entry.isIntersecting){
+          this.observer.disconnect();
+          this.intersected.emit();
+        }
+      }
+    },
+    {
+      root : null,
+      rootMargin : '0px',
+      threshold : 0
+    })
+
+    this.observer.observe(this.target.nativeElement);
+  }
+
+  ngOnDestroy(){
+    this.observer.disconnect();
+  }
+
+}

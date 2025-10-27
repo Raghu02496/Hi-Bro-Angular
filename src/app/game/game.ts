fb76dddb0724dee4e8675f6754189fbcc462f0cf
Raghu02496 <lawlieta023@gmail.com>
2025-10-27 13:35:19 +0530

    infinite scrolling with pagination added

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -2,11 +2,13 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../api-service';
 import { conversation, caseDetails, suspect } from '../detective.interface';
+import { InfiniteScroll } from '../directives/infinite-scroll';
 
 @Component({
   selector: 'game',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InfiniteScroll
   ],
   templateUrl: './game.html',
   styleUrl: './game.scss'
@@ -18,6 +20,8 @@ export class Game {
   interrogatingSuspect !: suspect
   loadingResult: boolean = false
   @ViewChild('responseBox',{read : ElementRef}) responseBox !: ElementRef<HTMLDivElement>
+  prevScrollTop = 0;
+  prevScrollHeight = 0;
   page_no = 0;
   loadMore = false
   constructor(
@@ -34,6 +38,7 @@ export class Game {
   }
 
   getConversation(){
+    this.observeResponseBoxMutations();
     this.page_no = 1;
     this.conversationArr = [];
     let request = {
@@ -54,6 +59,8 @@ export class Game {
 
   getOldMessages(){
     if(this.loadMore){
+
+      this.observeResponseBoxMutations();
       this.page_no ++;
       let request = {
         case_id : '68f8bb412f45862f36939609',
@@ -64,6 +71,8 @@ export class Game {
       this.apiService.getConversation(request).subscribe({
         next : (res:any)=>{
           if(res.ok){
+            this.prevScrollTop = this.responseBox.nativeElement.scrollTop;
+            this.prevScrollHeight = this.responseBox.nativeElement.scrollHeight;
             this.loadMore = res.data.length === 20
             this.conversationArr.unshift(...res.data)
           }
@@ -80,13 +89,15 @@ export class Game {
         suspect : this.interrogatingSuspect,
         content: content,
       }
-      this.conversationArr.push({ role: 'user', content: content })
+      this.conversationArr.push({_id : '', role: 'user', content: content })
       this.scrollToBottomOfResponseBox()
       this.loadingResult = true
       this.apiService.sendMessage(request).subscribe({
         next: (res: any) => {
           if (res.ok) {
-            this.conversationArr.push({ role: 'assistant', content: res.data })
+            const insertedIds = res.data.insertedIds;
+            this.conversationArr[this.conversationArr.length-1]._id = insertedIds[0];
+            this.conversationArr.push({_id : insertedIds[1], role: 'assistant', content: res.data.reply });
             this.scrollToBottomOfResponseBox()
           }
           this.loadingResult = false
@@ -102,7 +113,7 @@ export class Game {
     // To wait for browser to render the element
     setTimeout(()=>{
       this.responseBox.nativeElement.scrollTop = this.responseBox.nativeElement.scrollHeight
-    },50)
+    })
   }
 
   fetchCaseDetails(){
@@ -116,5 +127,16 @@ export class Game {
         }
       }
     })
+  }
+
+  observeResponseBoxMutations(){
+    const observer = new MutationObserver(()=>{
+      const newScrollHeight = this.responseBox.nativeElement.scrollHeight;
+      const delta = newScrollHeight - this.prevScrollHeight;
+      this.responseBox.nativeElement.scrollTop = this.prevScrollTop + delta;
+      observer.disconnect();
+    })
+
+    observer.observe(this.responseBox.nativeElement,{ childList: true, subtree: true });
   }
 }

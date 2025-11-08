8b211b6ba55b05e1a5506e0257abb393cf12ab44
Raghu02496 <lawlieta023@gmail.com>
2025-11-08 16:21:45 +0530

    code formatted

--- src/app/validators/validators.ts
+++ src/app/validators/validators.ts
@@ -1,13 +1,13 @@
 import { ValidatorFn, AbstractControl, ValidationErrors, FormControl } from "@angular/forms";
 
-export function whiteSpaceValidator() : ValidatorFn{
-    return (control : AbstractControl) : ValidationErrors | null =>{
-      const value = (control as FormControl).value.trim();
+export function whiteSpaceValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = (control as FormControl).value.trim();
 
-      if (value){
-        return null
-      }else{
-        return {emptyString : true}
-      }
+    if (value) {
+      return null
+    } else {
+      return { emptyString: true }
     }
   }
+}

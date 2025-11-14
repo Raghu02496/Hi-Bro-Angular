d79e89bad153387792ff087fbbbf120f5b7c7613
Raghu02496 <lawlieta023@gmail.com>
2025-11-14 11:23:03 +0530

    removed api hit on intervals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ApiService } from './api-service';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +12,5 @@ import { ApiService } from './api-service';
   styleUrl: './app.scss'
 })
 export class App {
-  constructor(private apiService : ApiService){
 
-  }
-
-  ngOnInit(){
-    setInterval(()=>{
-      this.apiService.status({}).subscribe();
-    }, 14 * 60 * 1000)
-  }
 }
